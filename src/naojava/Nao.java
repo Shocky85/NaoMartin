1a5ed652fe1f6fc69d19530bd6e5e137f1593a25
shocky85 <a.fouquet85@gmail.com>
2015-06-18 11:51:40 +0200

    it s ok

--- src/naojava/Nao.java
+++ src/naojava/Nao.java
@@ -14,77 +14,47 @@ public class Nao {
     private static String IP   = "172.16.6.117";
 
     static Session session;
-
     static ALLandMarkDetection markProxy;
     static ALTextToSpeech tts;
     static ALMotion motion;
     static ALRobotPosture posture;
     static ALMemory memProxy;
+    static NaoSpeech speech;
 
     public static void main(String[] args) throws Exception {
-
-        // Init session & connexion
         session        = new Session();
         Future<Void> future = session.connect("tcp://" + IP + ":" + PORT);
         future.get();
-
-        // Init nao features
         tts     = new ALTextToSpeech(session);
         motion  = new ALMotion(session);
         posture = new ALRobotPosture(session);
+        speech  = new NaoSpeech();
         int detectedLandmark = 0;
-
         tts.setLanguage("French");
         tts.say("Bonjour la France !");
         // motion.wakeUp();
-
-         markProxy = new ALLandMarkDetection(session);
-        markProxy.subscribe("LandmarkDetected", 500, (float) 0.0);
-        memProxy = new ALMemory(session);
-
-        tts.say("En attente de détection");
-        Object dataLandmark = memProxy.getData("LandmarkDetected");
-
-        System.out.println(dataLandmark.toString());
-        while(dataLandmark.toString()=="[]") {
-
-
-            markProxy.subscribe("LandmarkDetected", 500, (float) 0.0);
-            dataLandmark = memProxy.getData("LandmarkDetected");
-
-            if(dataLandmark.toString() != "[]") {
-                String values = dataLandmark.toString();
-                String[] results = values.split(",");
-                int id = Integer.parseInt(results[8].replace("[", "").replace("]", "").replace(" ",""));
-                detectedLandmark=id;
-            }
+        markProxy = new ALLandMarkDetection(session);
+        while(detectedLandmark == 0) {
+            detectedLandmark = detectLandmark();
+            tts.say("Landmark " + detectedLandmark + " détecté !");
+            NodeService sender = new NodeService();
+            int exit = sender.get("/page/" + detectedLandmark);
+            presentDestination(detectedLandmark);
+            System.out.println(exit);
+            detectedLandmark = 0;
         }
-        //int detectedLandmark = detectLandmark();
-         tts.say("Landmark " + detectedLandmark + " détecté !");
-
-
-        NodeService sender = new NodeService();
-        int exit = sender.get("/page/1");
-        System.out.println(exit);
-
     }
 
 
     public static int detectLandmark() throws Exception {
-
         markProxy.subscribe("LandmarkDetected", 500, (float) 0.0);
         memProxy = new ALMemory(session);
-
         tts.say("En attente de détection");
         Object dataLandmark = memProxy.getData("LandmarkDetected");
-
         System.out.println(dataLandmark.toString());
         while(dataLandmark.toString()=="[]") {
-
-
             markProxy.subscribe("LandmarkDetected", 500, (float) 0.0);
             dataLandmark = memProxy.getData("LandmarkDetected");
-
             if(dataLandmark.toString() != "[]") {
                 String values = dataLandmark.toString();
                 String[] results = values.split(",");
@@ -92,8 +62,26 @@ public class Nao {
                 return id;
             }
         }
-
         return 0;
     }
 
+    public static void presentDestination(int id) throws Exception{
+        switch(id) {
+            case 170:
+                tts.say(speech.getValleedessinges());
+                break;
+            case 130:
+                tts.say(speech.getPuydufou());
+                break;
+            case 68:
+                tts.say(speech.getZoodelafleche());
+                break;
+            case 187:
+                tts.say(speech.getPlanetesauvage());
+                break;
+            default:
+                tts.say("Destination inconnue !");
+                break;
+        }
+    }
 }
--- src/naojava/NodeService.java
+++ src/naojava/NodeService.java
@@ -9,7 +9,7 @@ import java.net.URL;
 
 public class NodeService {
 
-    private String apiUrl = "http://192.168.145.1:3000";
+    private String apiUrl = "http://172.16.113.106";
 
     protected int get(String path) throws IOException {
         HttpURLConnection con = sendData(path, "GET");
@@ -27,29 +27,16 @@ public class NodeService {
     }
 
     private void sendGet() throws Exception {
-
         URL obj = new URL(apiUrl);
         HttpURLConnection con = (HttpURLConnection)obj.openConnection();
-
-        // optional default is GET
         con.setRequestMethod("GET");
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + apiUrl);
-        System.out.println("Response Code : " + responseCode);
-
         BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
+        new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer response = new StringBuffer();
-
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
         in.close();
-
-        //print result
-        System.out.println(response.toString());
-
     }
 }
